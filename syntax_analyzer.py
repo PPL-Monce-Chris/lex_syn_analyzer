9bc6c9deb18b9643e10a05f6a70a5221a368bca9
chris <cjohn268@msudenver.edu>
2019-09-28 17:54:38 -0600

    added lexical aalyzer code

--- syntax_analyzer.py
+++ syntax_analyzer.py
@@ -10,6 +10,236 @@ Monce Romero
 from enum import Enum
 import sys
 
+
+# all char classes
+class CharClass(Enum):
+    EOF = 1
+    LETTER = 2
+    DIGIT = 3
+    OPERATOR_1 = 4
+    OPERATOR_2 = 5
+    PUNCTUATOR = 6
+    QUOTE = 7
+    BLANK = 8
+    OTHER = 9
+
+
+# reads the next char from input and returns its class
+def getChar(input):
+    if len(input) == 0:
+        return (None, CharClass.EOF)
+    c = input[0].lower()
+    if c.isalpha():
+        return (c, CharClass.LETTER)
+    if c.isdigit():
+        return (c, CharClass.DIGIT)
+    if c == '"':
+        return (c, CharClass.QUOTE)
+    if c in ['+', '-', '*', '/']:
+        return (c, CharClass.OPERATOR_1)
+    if c in ['=', '>', '<', '<=', '>=']:
+        return (c, CharClass.OPERATOR_2)
+    if c in ['.', ':', ',', ';']:
+        return (c, CharClass.PUNCTUATOR)
+    if c in [' ', '\n', '\t']:
+        return (c, CharClass.BLANK)
+    return (c, CharClass.OTHER)
+
+
+# calls getChar and getChar until it returns a non-blank
+def getNonBlank(input):
+    ignore = ""
+    while True:
+        c, charClass = getChar(input)
+        if charClass == CharClass.BLANK:
+            input, ignore = addChar(input, ignore)
+        else:
+            return input
+
+
+# adds the next char from input to lexeme, advancing the input by one char
+def addChar(input, lexeme):
+    if len(input) > 0:
+        lexeme += input[0]
+        input = input[1:]
+    return (input, lexeme)
+
+
+# all tokens
+class Token(Enum):
+    ADDITION = 1
+    ASSIGNMENT = 2
+    BEGIN = 3
+    BOOLEAN_TYPE = 4
+    COLON = 5
+    DO = 6
+    ELSE = 7
+    END = 8
+    EQUAL = 9
+    FALSE = 10
+    GREATER = 11
+    GREATER_EQUAL = 12
+    IDENTIFIER = 13
+    IF = 14
+    INTEGER_LITERAL = 15
+    INTEGER_TYPE = 16
+    LESS = 17
+    LESS_EQUAL = 18
+    MULTIPLICATION = 19
+    PERIOD = 20
+    PROGRAM = 21
+    READ = 22
+    SEMICOLON = 23
+    SUBTRACTION = 24
+    THEN = 25
+    TRUE = 26
+    VAR = 27
+    WHILE = 28
+    WRITE = 29
+
+
+# lexeme to token conversion
+
+# Special Words
+
+special = {
+    "integer": Token.INTEGER_TYPE,
+    "boolean": Token.BOOLEAN_TYPE,
+    "program": Token.PROGRAM,
+    "var": Token.VAR,
+    "begin": Token.BEGIN,
+    "while": Token.WHILE,
+    "do": Token.DO,
+    "if": Token.IF,
+    "else": Token.ELSE,
+    "end": Token.END,
+    "false": Token.FALSE,
+    "true": Token.TRUE,
+    "read": Token.READ,
+    "write": Token.WRITE,
+    "then": Token.THEN
+
+}
+
+# Punctuation
+
+punct = {
+    ":=": Token.ASSIGNMENT,
+    ":": Token.COLON,
+    ";": Token.SEMICOLON,
+    ".": Token.PERIOD
+}
+
+# Operations
+
+ops = {
+    "+": Token.ADDITION,
+    "-": Token.SUBTRACTION,
+    "*": Token.MULTIPLICATION
+}
+
+# Equivalence
+
+equal = {
+    "=": Token.EQUAL,
+    ">": Token.GREATER,
+    ">=": Token.GREATER_EQUAL,
+    "<": Token.LESS,
+    "<=": Token.LESS_EQUAL
+}
+
+
+# returns the next (lexeme, token) pair or None if EOF is reached
+def lex(input):
+    input = getNonBlank(input)
+
+    c, charClass = getChar(input)
+    lexeme = ""
+
+    # check EOF first
+    if charClass == CharClass.EOF:
+        return (input, None, None)
+
+    # Reading letters
+    if charClass == CharClass.LETTER:
+        while True:
+            input, lexeme = addChar(input, lexeme)
+            c, charClass = getChar(input)
+            if charClass == CharClass.DIGIT:
+                continue
+            elif lexeme in special:
+                return (input, lexeme, special[lexeme])
+            elif charClass != CharClass.LETTER and charClass != CharClass.DIGIT:
+                break
+
+        return (input, lexeme, Token.IDENTIFIER)
+
+    # Reading digits
+    if charClass == CharClass.DIGIT:
+        while True:
+            input, lexeme = addChar(input, lexeme)
+            c, charClass = getChar(input)
+            if charClass != CharClass.DIGIT:
+                break
+        return (input, lexeme, Token.INTEGER_LITERAL)
+
+    # Reading an operator
+    if charClass == CharClass.OPERATOR_1:
+        input, lexeme = addChar(input, lexeme)
+        if lexeme in ops:
+            return (input, lexeme, ops[lexeme])
+
+    if charClass == CharClass.OPERATOR_2:
+        while True:
+            input, lexeme = addChar(input, lexeme)
+            c, charClass = getChar(input)
+            if charClass != CharClass.OPERATOR_2:
+                break
+        return (input, lexeme, equal[lexeme])
+
+    # Reading a punctuation
+    if charClass == CharClass.PUNCTUATOR:
+        while True:
+            input, lexeme = addChar(input, lexeme)
+            c, charClass = getChar(input)
+            if charClass == CharClass.OPERATOR_2:
+                input, lexeme = addChar(input, lexeme)
+                return (input, lexeme, Token.ASSIGNMENT)
+            elif charClass != CharClass.PUNCTUATOR:
+                break
+        return (input, lexeme, punct[lexeme])
+
+    # TODO: anything else, raise an exception
+    raise Exception("Lexical Analyzer Error: unrecognized operator found")
+
+
+
+
+
+
+
+
+
+
+
+############@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@########################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # error messages to be used
 def errorMessage(code):
     msg = "Error " + str(code).zfill(2) + ": "
@@ -253,6 +483,40 @@ def parse(input, grammar, actions, gotos):
 # main
 if __name__ == "__main__":
 
+    # checks if source file was passed and if it exists
+    if len(sys.argv) != 2:
+        raise ValueError("Missing source file")
+    source = open(sys.argv[1], "rt")
+    if not source:
+        raise IOError("Couldn't open source file")
+    input = source.read()
+    source.close()
+    output = []
+
+    # main loop
+    while True:
+        input, lexeme, token = lex(input)
+        if lexeme == None:
+            break
+        output.append((lexeme, token))
+
+    # prints the output
+    for (lexeme, token) in output:
+        print(lexeme, token)
+
+
+
+
+
+
+
+
+
+
+
+
+
+    '''
     input = open("Grammar_new.txt", "rt")
     grammar = loadGrammar(input)
     input.close()
@@ -260,7 +524,7 @@ if __name__ == "__main__":
     input = open("slr_table_new.csv", "rt")
     actions, gotos = loadTable(input)
     input.close()
-
+    '''
 
 ##############################################################
     # this is to print out grammar actions and gotos to file
@@ -286,7 +550,7 @@ if __name__ == "__main__":
 
 
     # tree building update
-
+    '''
     tree = parse(input, grammar, actions, gotos)
     if tree:
         print("Input is syntactically correct!")
@@ -294,3 +558,4 @@ if __name__ == "__main__":
         tree.print()
     else:
         print("Code has syntax errors!")
+    '''
